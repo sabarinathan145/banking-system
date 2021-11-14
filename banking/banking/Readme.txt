7ed6f2b53605c90c7fae80935a2db2dd252b4226
sabarinathan145 <sabari.rishi34@gmail.com>
2021-11-14 19:44:57 +0530

    upload root project

---
+++ banking/banking/Readme.txt
@@ -0,0 +1,13 @@
+Admin:
+username:admin
+password:password123
+
+Users:
+user:ross
+password:password
+
+user:racheal
+password:password
+
+user:joey
+password:password
---
+++ banking/banking/net-banking/session_expired.php
@@ -0,0 +1,30 @@
+<?php
+    include "header.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="session_expired_style.css">
+</head>
+
+
+<body>
+    <div class="flex-container">
+        <div class="flex-item">
+            <img id="session" src="/images/hourglass.png">
+        </div>
+        <div class="flex-item-message">
+            <h1 id="session">Session Expired !</h1>
+            <p id="session">
+                c
+            </p>
+        </div>
+        <div class="flex-item">
+            <a href="./home.php" class="button">Go Home</a>
+        </div>
+    </div>
+
+</body>
+</html>
